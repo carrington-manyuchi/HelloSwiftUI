8af0d9a93f3f89de4cc7626da91a07592469c1fc
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-05-10 04:24:34 +0200

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Manyuchi, Carrington C on 2025/05/10.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
